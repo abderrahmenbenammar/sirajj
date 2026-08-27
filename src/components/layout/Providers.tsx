e457b09e57f1e457a0a2b0ff1812563136ec79e8
abderrahmenbenammar <abderrahmen.benammar@univ-tlemcen.dz>
2026-08-27 14:52:23 +0100

    Update Providers.tsx

--- src/components/layout/Providers.tsx
+++ src/components/layout/Providers.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "@/lib/theme-context";
 import { LangProvider } from "@/lib/lang-context";
 import { AuthProvider } from "@/lib/auth-context";
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
-    <ThemeProvider>
-      <LangProvider>
-        <AuthProvider>{children}</AuthProvider>
-      </LangProvider>
-    </ThemeProvider>
+    <SessionProvider>
+      <ThemeProvider>
+        <LangProvider>
+          <AuthProvider>{children}</AuthProvider>
+        </LangProvider>
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
